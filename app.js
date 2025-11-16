37bc491487b28910c4ec6ca18f6da8694727198f
Diego Aldao <diegofrontdev@gmail.com>
2025-11-15 22:22:18 -0300

    añadido el hero de la pagina principal,  añadido swiper

---
+++ app.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});

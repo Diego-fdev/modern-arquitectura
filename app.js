3c2e50f646536a8159483685368eb7ff0d8ed163
Diego Aldao <diegofrontdev@gmail.com>
2025-11-16 15:10:37 -0300

    añadida la seccion proyectos de la pagina principal

--- app.js
+++ app.js
@@ -13,3 +13,57 @@ const swiper2 = new Swiper(".swiper-about", {
   loop: true,
   autoplay: true,
 });
+
+const swiper3 = new Swiper(".swiper-trabajos", {
+  // Optional parameters
+  loop: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1536: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+/*FILTRO PROYECTOS*/
+
+const slidesProyectos = document.querySelectorAll(".slide-trabajo");
+const filtrosProyectos = document.querySelectorAll(".filtro-proyecto");
+
+const filterProyects = (filtro) => {
+  filtrosProyectos.forEach((filtroProyecto) => {
+    filtroProyecto.style.color = "black";
+  });
+  filtro.style.color = "#193cb8";
+  if (filtro.dataset.tipo === "todos") {
+    slidesProyectos.forEach((proyecto) => {
+      proyecto.style.display = "flex";
+    });
+  } else {
+    slidesProyectos.forEach((proyecto) => {
+      if (proyecto.dataset.tipo === filtro.dataset.tipo) {
+        proyecto.style.display = "flex";
+      } else {
+        proyecto.style.display = "none";
+      }
+    });
+  }
+};
+
+filtrosProyectos.forEach((filtro) => {
+  filtro.addEventListener("click", () => {
+    filterProyects(filtro);
+  });
+});
